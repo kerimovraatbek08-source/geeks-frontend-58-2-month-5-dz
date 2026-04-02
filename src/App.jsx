f9d9e81239b82c3cccc1a4f083e74cdf5420bcda
kerimovraatbek08-source <kerimovraatbek08@gmail.com>
2026-04-02 21:11:47 +0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Element from "./Components/Element";
+
+function App() {
+  return (
+    <div>
+      <h2>App.jsx</h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+        tempora, dignissimos possimus esse ut recusandae! Eveniet, ut magnam cum
+        unde ipsa molestiae veniam sequi error soluta, iste itaque repudiandae
+        quo! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Praesentium earum ea aliquid cupiditate ab inventore placeat, asperiores
+        aspernatur nisi minus, blanditiis nam obcaecati fuga doloremque odit
+        repellendus accusantium deserunt aperiam?
+      </p>
+      <Element />
+    </div>
+  );
+}
+
+export default App;
